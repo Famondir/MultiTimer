af1541ddacbd498719436716e7f7d5094bd22130
Famondir <schaefersd@gmail.com>
2022-11-20 10:33:31 +0100

    continuing splittin code

--- app/timer.js
+++ app/timer.js
@@ -5,6 +5,20 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const timer = {
+  time: 30*60,
+  players: 3,
+  timerlist: [],
+  state: "init",
+  activePlayer: 0
+}
+
+function setup() {
+  timer.timerlist = Array(timer.players).fill(timer.time);
+}
+
+setup();
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/timer.html");
 });
@@ -12,14 +26,16 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   console.log("a user connected");
 
+  io.emit("timer object transfer", timer);
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 
-  socket.on("chat message", (msg) => {
+/*   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
     io.emit("chat message", msg);
-  });
+  }); */
 });
 
 server.listen(3000, () => {
